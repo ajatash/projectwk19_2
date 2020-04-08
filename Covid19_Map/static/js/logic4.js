628f74dd624bc06533301aceb84a93cb4792eba2
carlreim <carlreim@gmail.com>
2020-04-08 17:16:43 -0500

    Added Mn Counties/Census data

--- Covid19_Map/static/js/logic4.js
+++ Covid19_Map/static/js/logic4.js
@@ -1,43 +1,3 @@
-// // Creating map object
-// var myMap = L.map("map", {
-//   center: [37.9487779,-119.7284779], //37.9487779,-119.7284779,6z
-//   zoom: 6,
-//   layers: [streetmap, earthquakes]
-// });
-//
-// var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.streets",
-//   accessToken: API_KEY
-// });
-//
-// var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.dark",
-//   accessToken: API_KEY
-// });
-// // Adding tile layer to the map
-// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.streets",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
-
-function chooseColor(magnitude){
-      var fillColor = ""
-      if ( magnitude > 8 ) fillColor = "#993404";
-      else if ( magnitude > 6 ) fillColor = "#d95f0e";
-      else if ( magnitude > 4 ) fillColor = "#fe9929e";
-      else if ( magnitude > 2 ) fillColor = "#fed98e";
-      // else if ( magnitude > 0 ) fillColor = "#ffffd4";
-      else fillColor = "#ffffd4";  // no data
-      return fillColor;
-    }
-
 function chooseRadius(confirmed){
       var radius = 0
       if ( confirmed > 10000 ) radius = 14;
@@ -52,16 +12,26 @@ function chooseRadius(confirmed){
 // Store API query variables
 var url = "COVID19_Cases_US.geojson";
 
-d3.json(url, function(response) {
-  var covidData = response.features;
-  console.log(covidData);
-  createFeatures(response.features)
-  });
+// d3.json(url, function(response) {
+//   var covidData = response.features;
+//   console.log(covidData);
+//   createFeatures(response.features)
+//   });
+//
+// d3.json("MN_counties.geojson", function(response){
+//   var countiesData = response.features;
+//   console.log(countiesData);
+//   createFeatures(response.features)
+//   });
 
-d3.json("MN_counties.geojson", function(response){
-  var countiesData = response.features;
-  console.log(countiesData);
-  createFeatures(response.features)
+d3.json("MN_counties.geojson", function(response1) {
+  d3.json(url, function(response2) {
+    var covidData = response2.features;
+    console.log(response2);
+    var countiesData = response1.features;
+    console.log(response1);
+    createFeatures(covidData, countiesData)
+    });
   });
 
 function createFeatures(covidData, countiesData) {
@@ -70,6 +40,19 @@ function createFeatures(covidData, countiesData) {
       "</h3><hr><p>Confirmed cases:" + (feature.properties.Confirmed) +
        "</p><hr><p>" + new Date(feature.properties.Last_Update) + "</p>");
   }
+  var mapStyle = {
+    color: "orange",
+    fillColor: "",
+    fillOpacity: 0,
+    weight: 2
+  };
+  var counties = L.geoJson(countiesData, {
+    // Passing in our style object
+    style: mapStyle,
+    onEachFeature: function( feature, layer ){
+      layer.bindPopup( "<h3>Population: " + feature.properties.Population + "</h3>")
+    }
+  });
   var covid = L.geoJson(covidData, {
     onEachFeature: oneachfeature,
     pointToLayer: function(feature,latlng){
@@ -81,16 +64,6 @@ function createFeatures(covidData, countiesData) {
       color: "#999"}); // feature.properties.mag
     }
   })//.addTo(myMap);
-  var mapStyle = {
-    color: "white",
-    fillColor: "pink",
-    fillOpacity: 0.5,
-    weight: 1.5
-  };
-  var counties = L.geoJson(countiesData, {
-    // Passing in our style object
-    style: mapStyle
-  });
 
 
 
---
+++ python/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,2 @@
+api_key = "b540517342f1e07a5145d767bd4355cba7bea8d1"
+# api_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
