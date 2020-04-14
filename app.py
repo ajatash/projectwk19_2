12c987aab139b3c29a54f5d15232049775d11b77
carlreim <carlreim@gmail.com>
2020-04-13 22:14:57 -0500

    working with geojson files in flask

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, send_file
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 
 
@@ -70,7 +70,16 @@ def counties():
         all_counties.append(counties_dict)
 
     return jsonify(all_counties)
-    return json_response(results)
+    # return json_response(results)
+
+@app.route('/geodata')
+# https://stackoverflow.com/questions/45910122/retriving-json-from-flask-endpoint
+def get_geojson():
+    path = 'data/MN_counties.geojson'
+    # file_path = os.path.join(parent_path, 'static\\js\\default_data.json')
+    with open(path, 'r') as file_data:
+        json_data = json.load(file_data)
+    return jsonify(json_data)
 
 
 
