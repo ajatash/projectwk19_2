0e9063cd70fc83ff5f6992376119ee104c59cf4a
ajatash <56736783+ajatash@users.noreply.github.com>
2020-04-17 16:57:08 -0500

    updating from group work

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import json
 from flask import Flask, jsonify, render_template, send_file, request, url_for, redirect
-from flask_json import FlaskJSON, JsonError, json_response, as_json
+#from flask_json import FlaskJSON, JsonError, json_response, as_json
 # from bson.json_util import dumps
 
 
@@ -29,9 +29,9 @@ from flask_json import FlaskJSON, JsonError, json_response, as_json
 
 # Use flask_pymongo to set up mongo connection
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/COVID_19"
-mongo = PyMongo(app)
+# app = Flask(__name__)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/COVID_19"
+# mongo = PyMongo(app)
 
 # Or set inline
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/COVID_19")
@@ -41,18 +41,18 @@ mongo = PyMongo(app)
 # Database Setup Postgres
 #################################################
 
-# connection_string = "postgres:postgres@localhost:5432/covid_db"
-# engine = create_engine(f'postgresql://{connection_string}')
+connection_string = "postgres:Marshall2020!@localhost:5432/covid_db"
+engine = create_engine(f'postgresql://{connection_string}')
 
 # reflect an existing database into a new model
-# Base = automap_base()
+Base = automap_base()
 # # reflect the tables
-# Base.prepare(engine, reflect=True)
+Base.prepare(engine, reflect=True)
 
 
 
 # Save reference to the table
-# Counties = Base.classes.counties
+Counties = Base.classes.counties
 # print(Counties)
 #################################################
 # Flask Setup
@@ -97,14 +97,15 @@ def counties():
 
     """Return a list of county data including the total population, population over 65, and percentage over 65"""
     # Query all passengers
-    results = session.query(Counties.pop, Counties.over_65_total, Counties.percent_over_65).all()
+    results = session.query(Counties.cty_name, Counties.pop, Counties.over_65_total, Counties.percent_over_65).all()
 
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_passengers
     all_counties = []
-    for pop, ovr65, pct65 in results:
+    for name, pop, ovr65, pct65 in results:
         counties_dict = {}
+        counties_dict["cty_name"] = name
         counties_dict["pop"] = pop
         counties_dict["over_65_total"] = ovr65
         counties_dict["percent_over_65"] = pct65
@@ -119,7 +120,7 @@ def get_county_geojson():
 
     # parent_path = '\\'.join(os.path.realpath(__file__).split('\\')[:-1])
     # file_path = os.path.join(parent_path, 'data\\MN_counties.geojson')
-    path = "data\\MN_counties.geojson"
+    path = "data/MN_counties.geojson"
     with open(path, 'r') as file_data:
         json_data = json.load(file_data)
     return jsonify(json_data)
@@ -130,7 +131,7 @@ def get_covid_geojson():
 
     # parent_path = '\\'.join(os.path.realpath(__file__).split('\\')[:-1])
     # file_path = os.path.join(parent_path, 'data\\COVID19_Cases_US.geojson')
-    path = "data\\COVID19_Cases_US.geojson"
+    path = "data/COVID19_Cases_US.geojson"
     with open(path, 'r') as file_data:
         json_data = json.load(file_data)
     return jsonify(json_data)
